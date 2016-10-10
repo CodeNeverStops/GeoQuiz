cc76583c71408aacfc1f4444c34932ed6d46a3ca
Wayne.You <lokiwuxi@gmail.com>
2016-10-10 14:07:39 +0800

    update chapter 2

---
+++ app/src/main/java/io/github/uv_lab/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package io.github.uv_lab.geoquiz;
+
+/**
+ * Created by youwei on 2016/10/10.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
